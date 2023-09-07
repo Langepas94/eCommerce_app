bd4c20bd65afa4d7bb7c2fdb031ad31c114a11b2
Artem <108129792+Langepas94@users.noreply.github.com>
2023-09-07 12:23:32 +0300

    add cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/widgets/widgets.dart';
 
+import '../../models/product_model.dart';
 
 class CartScreen extends StatelessWidget {
-
   static const String routeName = '/cart';
 
   static Route route() {
@@ -16,10 +16,147 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'Cart'
+      appBar: CustomAppBar(title: 'Cart'),
+      bottomNavigationBar: 
+      BottomAppBar(
+        color: Colors.black,
+        child: Container(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+             
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.white),
+                  onPressed: () {},
+                  child: Text(
+                    'Go to checkout',
+                    style: Theme.of(context).textTheme.headline3,
+                  ))
+            ],
+          ),
+        ),
       ),
-      // bottomNavigationBar: CustomNavBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Add \$20 for FREE Delivery',
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      child: Text(
+                        'Add more items',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5!
+                            .copyWith(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(),
+                        elevation: 0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CartProductCard(product: Product.products[0]),
+                CartProductCard(product: Product.products[2]),
+                  CartProductCard(product: Product.products[1]),
+              ],
+            ),
+            Column(
+              children: [
+                Divider(
+                  thickness: 2,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 40.0, vertical: 10.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Subtotal',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                          Text('\$5.98',
+                              style: Theme.of(context).textTheme.headline5)
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Delivery fee',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                          Text('\$2.98',
+                              style: Theme.of(context).textTheme.headline5)
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 60,
+                      decoration:
+                          BoxDecoration(color: Colors.black.withAlpha(50)),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      decoration: BoxDecoration(color: Colors.black),
+                      margin: const EdgeInsets.all(5.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline5!
+                                  .copyWith(color: Colors.white),
+                            ),
+                            Text('\$12.98',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(color: Colors.white))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/cart_product_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../../models/product_model.dart';
+
+class CartProductCard extends StatelessWidget {
+  final Product product;
+  const CartProductCard({
+    required this.product,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        children: [
+          Image.network(
+            product.imageURL,
+            width: 100,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  product.name,
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                Text(
+                  '\$${product.price}',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(width: 10),
+          Row(
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle)),
+              Text(
+                '1',
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              IconButton(onPressed: () {}, icon: Icon(Icons.add_circle)),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
